3ade5c1f0c8fc36933be22ea8cf568ed347de6a5
Kevin C Stone <kevin.c.stone@outlook.com>
2021-11-01 17:10:53 -0700

    added form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityValue: "",
+    };
+  }
+
+  handleClick = async () => {
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_EXPLORER_KEY}&q=${this.state.cityValue}&format=json`;
+
+    let response = await axios.get(url);
+    console.log(response.data[0]);
+    this.setState({ location: response.data[0] });
+  };
+
+  handleChange = (e) => {
+    this.setState({ cityValue: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.handleChange} value={this.state.cityValue} />
+        <p>{this.state.cityValue}</p>
+        <Button variant="success" onClick={this.handleClick}>
+          Explore!
+        </Button>
+        {this.state.location && <h1>{this.state.location.display_name}</h1>}
+      </div>
+    );
+  }
 }
-
-export default App;
