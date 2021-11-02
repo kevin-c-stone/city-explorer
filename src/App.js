b923954ebb8e1f30124ef81e7395e8847a4a585f
Kevin C Stone <kevin.c.stone@outlook.com>
2021-11-01 21:12:58 -0700

    tried getting map to render

--- src/App.js
+++ src/App.js
@@ -1,66 +1,58 @@
 import React, { Component } from "react";
 import axios from "axios";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Toast from "react-bootstrap/Toast";
-import ToastContainer from "react-bootstrap/ToastContainer";
+import Header from "./Header.js";
+import Map from "./Map.js";
+import Main from "./Main.js";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       cityValue: "",
+      mapValue: "",
     };
   }
 
-  handleClick = async () => {
+  handleClick = async (e) => {
+    try {
+      e.preventDefault();
+      await this.handleCoordinates();
+      this.handleMap();
+    } catch (error) {
+      this.setState({ error: true });
+    }
+  };
+
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({ cityValue: e.target.value });
+    this.setState({ mapValue: e.target.value });
+  };
+
+  handleCoordinates = async () => {
     const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_EXPLORER_KEY}&q=${this.state.cityValue}&format=json`;
 
     let response = await axios.get(url);
     this.setState({ location: response.data[0] });
   };
 
-  handleChange = (e) => {
-    this.setState({ cityValue: e.target.value });
+  handleMap = async () => {
+    const map = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_EXPLORER_KEY}&center=${this.state.location.lat},${this.state.location.lon}&zoom=15`;
+
+    let response = await axios.get(map);
+    this.setState({ mapValue: response });
   };
 
   render() {
     return (
       <div>
-        <input onChange={this.handleChange} value={this.state.cityValue} />
-        <p>{this.state.cityValue}</p>
-        <Button variant="success" onClick={this.handleClick}>
-          Explore!
-        </Button>
-        <ToastContainer>
-          <Toast>
-            <Toast.Header>
-              <img
-                src="holder.js/20x20?text=%20"
-                className="rounded me-2"
-                alt=""
-              />
-              <strong className="me-auto">Bootstrap</strong>
-              <small className="text-muted">just now</small>
-            </Toast.Header>
-            <Toast.Body>See? Just like this.</Toast.Body>
-          </Toast>
-          <Toast>
-            <Toast.Header>
-              <img
-                src="holder.js/20x20?text=%20"
-                className="rounded me-2"
-                alt=""
-              />
-              <strong className="me-auto">Bootstrap</strong>
-              <small className="text-muted">2 seconds ago</small>
-            </Toast.Header>
-            <Toast.Body>Heads up, toasts will stack automatically</Toast.Body>
-          </Toast>
-        </ToastContainer>
-        {this.state.location && <h1>{this.state.location.display_name}</h1>}
-        {this.state.location && <h1>{this.state.location.lat}</h1>}
-        {this.state.location && <h1>{this.state.location.lon}</h1>}
+        <Header />
+        <Main
+          handleClick={this.handleClick}
+          handleChange={this.handleChange}
+          cityValue={this.state.cityValue}
+        />
+        <Map mapValue={this.state.mapValue} />
       </div>
     );
   }
