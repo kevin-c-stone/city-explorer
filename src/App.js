6033bf400b65d1dbefa944307c89be9e6c50887c
Kevin C Stone <kevin.c.stone@outlook.com>
2021-11-01 18:34:28 -0700

    added some bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Toast from "react-bootstrap/Toast";
+import ToastContainer from "react-bootstrap/ToastContainer";
 
 export default class App extends Component {
   constructor(props) {
@@ -14,7 +17,6 @@ export default class App extends Component {
     const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_EXPLORER_KEY}&q=${this.state.cityValue}&format=json`;
 
     let response = await axios.get(url);
-    console.log(response.data[0]);
     this.setState({ location: response.data[0] });
   };
 
@@ -30,7 +32,35 @@ export default class App extends Component {
         <Button variant="success" onClick={this.handleClick}>
           Explore!
         </Button>
+        <ToastContainer>
+          <Toast>
+            <Toast.Header>
+              <img
+                src="holder.js/20x20?text=%20"
+                className="rounded me-2"
+                alt=""
+              />
+              <strong className="me-auto">Bootstrap</strong>
+              <small className="text-muted">just now</small>
+            </Toast.Header>
+            <Toast.Body>See? Just like this.</Toast.Body>
+          </Toast>
+          <Toast>
+            <Toast.Header>
+              <img
+                src="holder.js/20x20?text=%20"
+                className="rounded me-2"
+                alt=""
+              />
+              <strong className="me-auto">Bootstrap</strong>
+              <small className="text-muted">2 seconds ago</small>
+            </Toast.Header>
+            <Toast.Body>Heads up, toasts will stack automatically</Toast.Body>
+          </Toast>
+        </ToastContainer>
         {this.state.location && <h1>{this.state.location.display_name}</h1>}
+        {this.state.location && <h1>{this.state.location.lat}</h1>}
+        {this.state.location && <h1>{this.state.location.lon}</h1>}
       </div>
     );
   }
